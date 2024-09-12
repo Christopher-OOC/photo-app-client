d0cc3e68f457ba78f6ae2e420ea85e330bd106ad
Christopher-OOC <olojedechristopher24@gmail.com>
2024-09-12 17:47:27 +0100

    comit

--- src/main/java/com/example/thymeleaf/ThymeleafTest/ThymeleafTestApplication.java
+++ src/main/java/com/example/thymeleaf/ThymeleafTest/ThymeleafTestApplication.java
@@ -2,12 +2,38 @@ package com.example.thymeleaf.ThymeleafTest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class ThymeleafTestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafTestApplication.class, args);
+	}
+
+	@Bean
+	RestTemplate getRestTemplate() {
+
+		return new RestTemplate();
+	}
+
+	@Bean
+	WebClient getWebClient(ClientRegistrationRepository clientRegistrationRepository,
+						   OAuth2AuthorizedClientRepository oAuth2AuthorizedClientRepository) {
+
+		var oauht2 = new ServletOAuth2AuthorizedClientExchangeFilterFunction(
+				clientRegistrationRepository,
+				oAuth2AuthorizedClientRepository
+		);
+
+		oauht2.setDefaultOAuth2AuthorizedClient(true);
+
+		return WebClient.builder().apply(oauht2.oauth2Configuration()).build();
 	}
 
 }
--- src/main/java/com/example/thymeleaf/ThymeleafTest/controllers/AlbumsController.java
+++ src/main/java/com/example/thymeleaf/ThymeleafTest/controllers/AlbumsController.java
@@ -2,6 +2,11 @@ package com.example.thymeleaf.ThymeleafTest.controllers;
 
 import com.example.thymeleaf.ThymeleafTest.response.AlbumRest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -13,14 +18,23 @@ import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class AlbumsController {
 
     @Autowired
     private OAuth2AuthorizedClientService auth2ClientService;
+
+//    @Autowired
+//    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient webClient;
 
     @GetMapping("/albums")
     public String getAlbums(Model model, @AuthenticationPrincipal OidcUser principal) {
@@ -41,18 +55,20 @@ public class AlbumsController {
         System.out.println("Token Value = " + tokenValue);
         System.out.println("Authorities Value = " + principal.getAuthorities());
 
-        AlbumRest album1 = new AlbumRest();
-        album1.setAlbumId("albumOne");
-        album1.setAlbumTitle("Album one title");
-        album1.setAlbumUrl("http://localhost:8082/albums/1");
 
-        AlbumRest album2 = new AlbumRest();
-        album2.setAlbumId("albumTwo");
-        album2.setAlbumTitle("Album two title");
-        album2.setAlbumUrl("http://localhost:8082/albums/2");
+        String url = "http://localhost:8082/albums";
 
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "Bearer " + jwtAccessToken);
 
-        model.addAttribute("albums", Arrays.asList(album1, album2));
+        HttpEntity<List<AlbumRest>> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<List<AlbumRest>> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, new ParameterizedTypeReference<>() {
+        });
+
+        List<AlbumRest> albums = responseEntity.getBody();
+
+          model.addAttribute("albums", albums);
 
         return "main";
     }
